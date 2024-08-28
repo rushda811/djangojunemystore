cc72755ec3588b55143d8fa4869506336d695fc4
Fathima Rushda <fathimarushda811@gmail.com>
2024-08-28 10:02:21 +0530

    ok

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class products(models.Model):
+    name=models.CharField(unique=True,max_length=200)
+    price=models.PositiveIntegerField()
+    discription=models.CharField(max_length=200)
+    category=models.CharField(max_length=200)
+    image=models.ImageField(null=True)
+
+    def __str__(self):
+        return self.name
